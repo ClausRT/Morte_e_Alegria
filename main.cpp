5c19006978d291251e0df17aa929f87f3b2b00e7
Claus Tessmann <clausrt@outlook.com>
2017-05-07 13:30:39 -0300

    Revert "Revert "Exportar para CSV, consertado temperatura mínima e máximo.""
    This reverts commit b88abfa6ca360d760aab664e2854ac35bee3655f.

--- main.cpp
+++ main.cpp
@@ -9,31 +9,24 @@ int main(void)
 //	cout << "Iniciando o programa." << endl;
 
 	Placa* kitPIC = new Placa(3);	//Comunicação serial na COM3
-	Monitoramento* monitor = new Monitoramento(kitPIC, 1000); 	//2 segundos entre as leituras
+	Monitoramento* monitor = new Monitoramento(kitPIC, 1000);
 
 	cout << "Iniciada a leitura da temperatura." << endl;
-
-	monitor->setTemperaturaMaxima(24.2);
-	monitor->setTemperaturaMinima(15.8);
-	monitor->lerContinuamente(true);
-	//monitor->salvarEmDisco();
+	monitor->setTemperaturaMaxima(20);
+	monitor->setTemperaturaMinima(15);
+	monitor->leitura();
 
 	ListaEncadeada<Dado> lista = monitor->getLeituras();
-	cout << "Numero de leituras: " << lista.getTam() << endl;
-	Sleep(10000);
-	lista = monitor->getLeituras();
-	cout << "Numero de leituras apos 10s: " << lista.getTam() << endl;
-
 	Dado lido;
 	for (int i = 0; i < lista.getTam(); i++){
 		lido = lista.pos(i);
-		cout << lido.temperatura << "  " << lido.dataFormatada() << endl;
+		cout << lido.temperatura << " Resistor: " << lido.resistor << " Ventoinha: " << lido.ventoinha << "  " << lido.dataFormatada() << endl;
 	}
-
+	cout << "Leituras: " << lista.getTam() << endl;
 	cout << "Leitura terminada." << endl;
-
 	cout << "Esse é o fim do programa." << endl;
-    delete monitor;
+	monitor->salvarEmDisco();
+
 	return 0;
 
 }
